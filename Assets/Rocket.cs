f5267e2aa92447e24d16d7b3bc2b8efb67226897
Josh Landron <jlandron@uw.edu>
2019-07-04 23:26:46 -0700

    Basics of Level one

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 
 public class Rocket : MonoBehaviour {
+    [SerializeField] float thrust = 100f;  //<-[] Allows the Unity engine to activly adjust this value
+    [SerializeField] float RCSthrust = 100f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
     // Start is called before the first frame update
@@ -15,8 +18,21 @@ public class Rocket : MonoBehaviour {
     }
 
     private void ProcessInput( ) {
+        Thrust( );
+        Rotate( );
+    }
+    void OnCollisionEnter( Collision collision ) {
+        switch (collision.gameObject.tag){
+            case "Friendly":
+                break;
+            default:
+                print( "ded" );
+                break;
+        }
+    }
+    private void Thrust( ) {
         if( Input.GetKey( KeyCode.Space ) ) {
-            rigidBody.AddRelativeForce( Vector3.up );
+            rigidBody.AddRelativeForce( Vector3.up * thrust * Time.smoothDeltaTime );
             if( !audioSource.isPlaying ) {
                 audioSource.Play( );
             }
@@ -24,11 +40,15 @@ public class Rocket : MonoBehaviour {
         else {
             audioSource.Stop( );
         }
+    }
+    private void Rotate( ) {
+        rigidBody.freezeRotation = true; //take control of rotation
         if( Input.GetKey( KeyCode.A ) ) {
-            transform.Rotate( Vector3.forward );
+            transform.Rotate( Vector3.forward * RCSthrust * Time.smoothDeltaTime );
         }
         else if( Input.GetKey( KeyCode.D ) ) {
-            transform.Rotate( Vector3.back );
+            transform.Rotate( -Vector3.forward * RCSthrust * Time.smoothDeltaTime );
         }
+        rigidBody.freezeRotation = false; //release control of rotation
     }
 }
